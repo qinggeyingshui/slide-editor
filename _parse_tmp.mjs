79530ac15866f3455adbd9a25d47fd9c4b8bd6a0
qinggeyingshui <19973331516@163.com>
2026-05-23 00:09:11 +0800

    chore: remove redundant files (~76MB)

--- _parse_tmp.mjs
+++
@@ -1,7 +0,0 @@
-
-import { parse } from './_pptxtojson_bundle.mjs';
-import fs from 'fs';
-const buf = fs.readFileSync('E:/2026/x-fudan/new/GenericAgent/temp/蓝色-保研推免自我陈述顶部导航-无校徽版本.pptx');
-const r = await parse(buf);
-fs.writeFileSync('E:/2026/x-fudan/new/GenericAgent/temp/pptx_parsed.json', JSON.stringify(r));
-console.log('parsed');
